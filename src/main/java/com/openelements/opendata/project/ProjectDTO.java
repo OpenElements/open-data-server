e4e77665f9630630b1adf9db12d06b3fd57ff38c
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-04-08 19:50:53 +0200

    super basic and dummy way to load logos

--- src/main/java/com/openelements/opendata/project/ProjectDTO.java
+++ src/main/java/com/openelements/opendata/project/ProjectDTO.java
@@ -5,13 +5,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Objects;
 import java.util.Set;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 @Schema(title = "Project", description = "An open source project")
 public record ProjectDTO(
         @NonNull @Schema(description = "UUID of the project", example = "b14763b0-60e0-4861-ba30-53f17cc4471e", required = true) String uuid,
         @NonNull @Schema(description = "Name of the project", example = "Spring Boot", required = true) String name,
         @NonNull @Schema(description = "Description of the project", example = "A HTTP library to connect tio GitHub", required = true) String description,
-        @NonNull @Schema(description = "Path to the logo of the project", example = "TODO", required = true) String logoPath,
+        @Nullable @Schema(description = "Path to the logo of the project", example = "TODO", required = false) String svgLogoForBrightBackground,
+        @Nullable @Schema(description = "Path to the logo of the project", example = "TODO", required = false) String svgLogoForDarkBackground,
+        @Nullable @Schema(description = "Path to the logo of the project", example = "TODO", required = false) String pngLogoForBrightBackground,
+        @Nullable @Schema(description = "Path to the logo of the project", example = "TODO", required = false) String pngLogoForDarkBackground,
         @NonNull @Schema(description = "Set of repository names that match to the project", example = "TODO", required = true) Set<String> matchingRepos) implements
         DTO {
 
@@ -19,7 +23,6 @@ public record ProjectDTO(
         Objects.requireNonNull(uuid, "uuid cannot be null");
         Objects.requireNonNull(name, "name cannot be null");
         Objects.requireNonNull(description, "description cannot be null");
-        Objects.requireNonNull(logoPath, "logoPath cannot be null");
         Objects.requireNonNull(matchingRepos, "matchingRepos cannot be null");
     }
 }
--- src/main/java/com/openelements/opendata/project/ProjectService.java
+++ src/main/java/com/openelements/opendata/project/ProjectService.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.openelements.opendata.base.AbstractService;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
@@ -34,13 +38,43 @@ public class ProjectService extends AbstractService<ProjectDTO> {
                         final String uuid = PROJECT_UUID_PREFIX + node.get("identifier").asText();
                         final String name = node.get("name").asText();
                         final String description = node.get("description").asText();
-                        final String logoPath = node.get("logo").asText();
+                        final Function<String, String> base64ImageSupplier = fieldName -> {
+                            if (node.has(fieldName)) {
+                                final String imageFile = node.get(fieldName).asText();
+                                try (final InputStream inputStream = ProjectService.class.getClassLoader()
+                                        .getResourceAsStream("logos/" + imageFile)) {
+                                    try (final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                                        final byte[] buffer = new byte[8192];
+                                        int bytesRead;
+                                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                                            outputStream.write(buffer, 0, bytesRead);
+                                        }
+                                        final byte[] inputBytes = outputStream.toByteArray();
+                                        return Base64.getEncoder().encodeToString(inputBytes);
+                                    }
+                                } catch (Exception e) {
+                                    throw new RuntimeException("Error in reading stream", e);
+                                }
+                            } else {
+                                return null;
+                            }
+                        };
+                        final String svgLogoForBrightBackground = base64ImageSupplier.apply(
+                                "svgLogoForBrightBackground");
+                        final String svgLogoForDarkBackground = base64ImageSupplier.apply(
+                                "svgLogoForDarkBackground");
+                        final String pngLogoForBrightBackground = base64ImageSupplier.apply(
+                                "pngLogoForBrightBackground");
+                        final String pngLogoForDarkBackground = base64ImageSupplier.apply(
+                                "pngLogoForDarkBackground");
+
                         final Set<String> matchingRepos = new HashSet<>();
                         final ArrayNode matchingReposNode = (ArrayNode) node.get("repositories");
                         matchingReposNode.forEach(matchingRepoNode -> {
                             matchingRepos.add(matchingRepoNode.asText());
                         });
-                        result.add(new ProjectDTO(uuid, name, description, logoPath,
+                        result.add(new ProjectDTO(uuid, name, description, svgLogoForBrightBackground,
+                                svgLogoForDarkBackground, pngLogoForBrightBackground, pngLogoForDarkBackground,
                                 Collections.unmodifiableSet(matchingRepos)));
                     }
                 });
@@ -49,5 +83,9 @@ public class ProjectService extends AbstractService<ProjectDTO> {
         } catch (final Exception e) {
             throw new RuntimeException("Error", e);
         }
+    }
+
+    private String getImageAsBase64(final String name) {
+        return null;
     }
 }
