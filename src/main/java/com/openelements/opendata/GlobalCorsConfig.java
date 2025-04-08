dc4cdd16da7767a7e69c8b37380156b7d8d92d25
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-04-08 11:44:54 +0200

    CORS

---
+++ src/main/java/com/openelements/opendata/GlobalCorsConfig.java
@@ -0,0 +1,18 @@
+package com.openelements.opendata;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS settings to all paths
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("*") // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*") // Allow all request headers
+                .allowCredentials(false); // Disable credentials (cookies, authorization headers, etc.)
+    }
+}
